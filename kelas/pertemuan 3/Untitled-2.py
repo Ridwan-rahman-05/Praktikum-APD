7c8e4408b23871c0a49b77808b95857365ceec8e
Ridwan-rahman-05 <rahmanpokeld96@gmail.com>
2024-09-20 13:59:10 +0700

    Add files via upload

---
+++ kelas/pertemuan 3/Untitled-2.py
@@ -0,0 +1,11 @@
+print("menu")
+print("1.nonton film")
+print("2.ngoding")
+print("3.keluar")
+menu = int(input("menu="))
+if menu == 1:
+     print("1.nonton film")
+elif menu == 2:
+     print("2.ngoding")
+elif menu == 3:
+     print("3.keluar")

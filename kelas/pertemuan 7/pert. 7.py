f73ea83dbd0cb65a43b59e8ed5f5476ed5a06fe2
Ridwan-rahman-05 <rahmanpokeld96@gmail.com>
2024-10-18 14:11:18 +0700

    Add files via upload

---
+++ kelas/pertemuan 7/pert. 7.py
@@ -0,0 +1,45 @@
+#def hasil(nama):     #(nama) = parameter
+#    nama
+#   print("Hello world " + nama)
+
+#hasil("adi")   #argumen
+#hasil("adri")
+#hasil("ridwan")
+
+#def luas_persegi_panjang(panjang,lebar=5):
+#    luas = panjang*lebar
+#    print(f"luas peregi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}")
+#    return luas
+#luas_persegi_panjang(10,2)  #menimpa nilai lebar = 5
+#print(luas_persegi_panjang)
+
+#nama = "dimas"  #variabel global
+
+#def say_Hello():
+#    nama = "daffa"   #variabel lokal
+#    print(nama,"dalam fungsi")
+
+#print(nama,"luar fungsi")
+#print(say_Hello)
+
+#def faktorial(n):
+#    if n == 1:
+#        return 1
+#    else:
+#        return n *faktorial (n-1)
+
+#print(faktorial(12))
+
+def say_hello():
+    nama = "dimas"
+    print("hello")
+    return nama
+
+say_hello()
+
+#import os
+#def say_hello():
+   # while True:
+  #      pilihan = input("1. untuk berhenti 2.lanjut")
+ #       if pilihan == "1"
+#            return pilihan
